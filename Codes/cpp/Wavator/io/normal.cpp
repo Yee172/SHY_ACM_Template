efd946f67e63267ade3bcd5c81400b9f28b4666b
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-11 05:31:32 +0800

    Update normal.cpp

--- Codes/cpp/Wavator/io/normal.cpp
+++ Codes/cpp/Wavator/io/normal.cpp
@@ -10,7 +10,7 @@ template <typename T> inline bool read(T &x) {
             f ^= true;
     }
     for (x = 0; isdigit(c); c = gc) {
-        x = x * 10 + c - 48;
+        x = x << 1 + x << 3 + x ^ 48;
     }
     if (f) {
         x = -x;
