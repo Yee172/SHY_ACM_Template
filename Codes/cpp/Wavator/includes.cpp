a3c4021348ec6521a20a6261314e3e42006ab1ca
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-12 22:03:26 +0800

    Update includes.cpp

--- Codes/cpp/Wavator/includes.cpp
+++ Codes/cpp/Wavator/includes.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <ctime>
 #include <queue>
+#include <stack>
 #include <cstdio>
 #include <string>
 #include <vector>
@@ -12,6 +13,7 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <functional>
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
