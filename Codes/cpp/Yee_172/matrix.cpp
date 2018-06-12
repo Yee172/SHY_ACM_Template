f5a7128358872f6568898553c737715613f8990e
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-06-12 21:01:54 +0800

    update matrix.cpp

--- Codes/cpp/Yee_172/matrix.cpp
+++ Codes/cpp/Yee_172/matrix.cpp
@@ -1,61 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll N = 110;
-const ll mod = 0x3b9aca07;
+#define mod 0x3b9aca07
 #define rep(i, n) for (ll i = 0; (i) < (n); (i)++)
 
-struct Matrix
+template <typename T>
+class Matrix
 {
-    bool cyclic = false;
+public:
     ll n;
-    ll a[N][N];
-    Matrix() {}
+    bool cyclic = false;
+    vector<vector<T> > a;
     Matrix(ll n) : n(n)
     {
         init();
     }
-    void init()
-    {
-        rep(i, n) rep(j, n) a[i][j] = 0;
-    }
-    ll *operator[](ll i)
+    vector<T> &operator[](ll i)
     {
         return a[i];
     }
-    Matrix operator*(Matrix &b) const
+
+    Matrix<T> operator*(Matrix &b) const
     {
-        Matrix res(n);
+        Matrix<T> res(n);
         if (cyclic)
         {
             res.cyclic = true;
-            rep(k, n) if (a[0][k]) rep(j, n) if (b[k][j]) res[0][j] += a[0][k] * b[k][j] % mod, res[0][j] %= mod;
+            rep(k, n) if (a[0][k]) rep(j, n) if (b[k][j])
+                        {
+#ifdef mod
+                            res[0][j] += a[0][k] * b[k][j] % mod, res[0][j] %= mod;
+#else
+                            res[0][j] += a[0][k] * b[k][j];
+#endif
+                        }
             rep(i, n - 1)
             {
                 res[i + 1][0] = res[i][n - 1];
                 rep(j, n - 1) res[i + 1][j + 1] = res[i][j];
             }
         }
-        else rep(i, n) rep(j, n) rep(k, n) res[i][j] += a[i][k] * b[k][j] % mod, res[i][j] %= mod;
+        else rep(i, n) rep(j, n) rep(k, n)
+                    {
+#ifdef mod
+                        res[i][j] += a[i][k] * b[k][j] % mod, res[i][j] %= mod;
+#else
+                        res[i][j] += a[i][k] * b[k][j];
+#endif
+                    }
         return res;
     }
-    Matrix operator^(ll t) const
+
+    Matrix<T> operator^(ll t) const
     {
-        Matrix res(n);
+        Matrix<T> res(n);
         res.cyclic = cyclic;
         rep(i, n) res[i][i] = 1;
-        Matrix tmp = *this;
+        Matrix<T> tmp = *this;
         while (t)
         {
-            if (t & 1)
-                res = res * tmp;
+            if (t & 1) res = res * tmp;
             t >>= 1;
             tmp = tmp * tmp;
         }
         return res;
-    }
+    };
+
     void print()
     {
-        rep(i, n) rep(j, n) printf("%lld%c", a[i][j], j == n - 1 ? '\n' : ' ');
-    }
+        rep(i, n) rep(j, n) cout << a[i][j] << j == n - 1 ? '\n' : ' ';
+    };
+
+private:
+    void init()
+    {
+        a = vector<vector<T> >((unsigned long long) n, vector<T>((unsigned long long) n, 0));
+        rep(i, n) rep(j, n) a[i][j] = 0;
+    };
 };
