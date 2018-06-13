6cd31f06cf9603088e7d71548b7bf646739f11d3
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-13 23:05:57 +0800

    Update dsu.cpp

--- Codes/cpp/Wavator/data_struct/dsu.cpp
+++ Codes/cpp/Wavator/data_struct/dsu.cpp
@@ -1,16 +1,18 @@
-struct DSU {
-#define DSU_SZ 100002
-    int p[DSU_SZ],r[DSU_SZ];
-    void init(int n) {
+class DSU {
+public:
+    vector<int> p, r;
+    virtual void init(int n) {
+        p.resize(n+1);
+        r.resize(n+1);
         for(int i = 0; i <= n; i++) {
             p[i] = i;
             r[i] = 0;
         }
     }
-    int find(int x) {
+    virtual int find(int x) {
         return p[x] == x? x: p[x] = find(p[x]);
     }
-    bool unite(int x,int y) {
+    virtual bool unite(int x,int y) {
         x = find(x);
         y = find(y);
         if(x == y)
@@ -24,9 +26,7 @@ struct DSU {
         }
         return true;
     }
-    bool same(int x,int y) {
+    virtual bool same(int x,int y) {
         return find(x)==find(y);
     }
-
-#undef DSU_SZ
 };
