051c0a63f6d973a659a1080a1d3bcbb7071bfce0
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-29 14:35:50 +0800

    Add files via upload

--- Codes/cpp/Wavator/io/2B.cpp
+++ Codes/cpp/Wavator/io/2B.cpp
@@ -1,21 +1,50 @@
-char OOXOO[1<<20],*XXOO=OOXOO,*OOXX=OOXOO;
-#define gc (XXOO == OOXX && (OOXX = (XXOO = OOXOO) + fread(OOXOO, 1, 1 << 20, stdin), XXOO == OOXX)? -1: *XXOO++)
-
+inline char gc() {
+    static char buf[1<<20], *head = buf, *tail = buf;
+    return (head == tail && (tail = (head = buf) + fread_unlocked(buf, 1, 1 << 20, stdin), head == tail)? -1: *head++);
+}
 template <typename T> inline bool read(T &x) {
-    bool f = false;
-    char c;
-    for (c = gc; !isdigit(c); c = gc) {
+    static bool f;
+    static char c;
+    for (c = gc(), f = false; !isdigit(c); c = gc()) {
         if (c == EOF)
             return false;
-        if (c == '-')
+        else if (c == 45)
             f = true;
     }
-    for (x = 0; isdigit(c); c = gc) {
+    for (x = 0; isdigit(c); c = gc())
         x = x * 10 + c - 48;
-    }
-    if (f) {
+    if (f)
         x = -x;
-    }
+    return true;
 }
-
-#undef gc
+inline bool read(double &x) {
+    static char c;
+    static bool f;
+    for(f = false; (c = gc()) != '-' && !isdigit(c);)
+        if (c == EOF) return false;
+    if (c == '-')
+        x = 0, f = true;
+    else
+        x = c & 15;
+    for (;isdigit(c = gc());)
+        x = x * 10 + (c & 15);
+    if (c == '.') {
+        double base = 1.0;
+        for (;isdigit(c = gc());)
+            x += (c & 15) * (base /= 10);
+    }
+    if (f)
+        x = -x;
+    return true;
+}
+inline bool read(char s[]) {
+    static int ptr;
+    static char ch;
+    for (ch = gc(), ptr = 0; ch != ' ' && ch != '\n'; ch = gc()) {
+        if (ch == EOF)
+            return false;
+        s[ptr++] = ch;
+    }
+    s[ptr] = 0;
+    return true;
+}
