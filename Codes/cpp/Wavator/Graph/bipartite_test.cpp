f8a9b06c76d775d1fabf24269ffb1aae3ff6f42e
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-06-29 14:57:11 +0800

    emmm

--- Codes/cpp/Wavator/Graph/bipartite_test.cpp
+++ Codes/cpp/Wavator/Graph/bipartite_test.cpp
@@ -1,21 +1,21 @@
-    int Color[N];
-    bool dfs(int u, int co) {
-        Color[u] = co;
-        int v;
-        edge * t;
-        for (t = li[u]; t; t = t->next) {
-            v = t->y;
-            if (!Color[v] && !dfs(v, -co))
-                return false;
-            if (Color[u] == Color[v])
-                return false;
-        }
-        return true;
+const int N = 100005;
+vector<int> e[N];
+int c[N];
+bool dfs(int u, int co) {
+    c[u] = co;
+    for (auto &v: e[u]) {
+        if (!c[v] && !dfs(v, -co))
+            return false;
+        if (c[u] == c[v])
+            return false;
     }
-    bool check(int n) {
-        memset(Color, 0, sizeof(Color));
-        for (int i = 1; i <= n; ++i)
-            if (Color[i] == 0 && !dfs(i, 1))
-                return false;
-        return true;
-    }
+    return true;
+}
+
+bool check(int n) {
+    fill(c + 1, c + n + 1, 0);
+    for (int i = 1; i <= n; ++i)
+        if (c[i] == 0 && !dfs(i, 1))
+            return false;
+    return true;
+}
