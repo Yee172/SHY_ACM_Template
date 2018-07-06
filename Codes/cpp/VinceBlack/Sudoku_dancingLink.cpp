e574132d1ab7e4f361399cc158d6635ab9a0a8f7
ArchyCillp <11610809@mail.sustc.edu.cn>
2018-07-07 03:37:06 +0800

    comment

--- Codes/cpp/VinceBlack/Sudoku_dancingLink.cpp
+++ Codes/cpp/VinceBlack/Sudoku_dancingLink.cpp
@@ -1,3 +1,27 @@
+/*
+Solving problems in 9x9 normal sudoku game rules;
+input like this:
+0 0 8 0 0 0 2 0 0
+0 3 0 8 0 2 0 6 0
+7 0 0 0 9 0 0 0 5
+0 5 0 0 0 0 0 1 0
+0 0 4 0 0 0 6 0 0
+0 2 0 0 0 0 0 7 0
+4 0 0 0 8 0 0 0 6
+0 7 0 1 0 3 0 9 0
+0 0 1 0 0 0 8 0 0
+output like this:
+6 1 8 7 3 5 2 4 9
+5 3 9 8 4 2 7 6 1
+7 4 2 6 9 1 3 8 5
+3 5 7 4 2 6 9 1 8
+1 8 4 5 7 9 6 2 3
+9 2 6 3 1 8 5 7 4
+4 9 3 2 8 7 1 5 6
+8 7 5 1 6 3 4 9 2
+2 6 1 9 5 4 8 3 7
+(having no end condition(while(1)))
+*/
 #include<bits/stdc++.h>
 #define P pair<int,pair<int,int> >
 #define MAXNODE 300010
