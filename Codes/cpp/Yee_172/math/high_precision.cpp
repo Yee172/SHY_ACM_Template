b17a324741cc7c449f53400f871b283a6b33d696
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-09-20 15:18:21 +0800

    Update

--- Codes/cpp/Yee_172/math/high_precision.cpp
+++ Codes/cpp/Yee_172/math/high_precision.cpp
@@ -4,10 +4,13 @@ typedef long long ll;
 
 namespace high_precision
 {
-    const int INITIAL_SIZE = 500;       // sizeof(BigInteger) = sizeof(int) * (UNITIAL_SIZE + 1)
-    const int UNIT_LENGTH = 4;          // Maximum length of BigInteger = INITIAL_SIZE * UNIT_LENGTH
-    const int UNIT_SIZE = 10000;        // 10 ** UNIT_LENGTH
+    const int INITIAL_SIZE = 500;                 // sizeof(BigInteger) = sizeof(int) * (UNITIAL_SIZE + 1)
+    const int UNIT_LENGTH = 8;                    // Maximum length of BigInteger = INITIAL_SIZE * UNIT_LENGTH
+    const int UNIT_SIZE = 100000000;              // 10 ** UNIT_LENGTH
     const int UNIT_MAX = UNIT_SIZE - 1;
+    char buffer[INITIAL_SIZE * UNIT_LENGTH + 1];  // inner char array for BigInteger2charArray
+    char raw_sqrt[INITIAL_SIZE * UNIT_LENGTH + 1];
+    int SQRT_L, SQRT_CNT;
 
     class BigInteger
     {
@@ -15,13 +18,13 @@ namespace high_precision
         int a[INITIAL_SIZE];
         int length;
 
-        BigInteger() : a() { length = 0; memset(a, 0, sizeof(a)); }
-        explicit BigInteger(long long);
-        explicit BigInteger(int);
+        BigInteger() : a() { length = 1; memset(a, 0, sizeof(a)); }
+        BigInteger(long long);
+        BigInteger(int);
         explicit BigInteger(const char *);
         BigInteger(const BigInteger &T) : a(), length(T.length) { memcpy(a, T.a, sizeof a); }
 
-        BigInteger &operator =  (const BigInteger &T) { length = T.length; memcpy(a, T.a, sizeof a); return *this; };
+        BigInteger &operator =  (const BigInteger &T) { length = T.length; memcpy(a, T.a, sizeof a); return *this; }
         BigInteger  operator +  (const BigInteger &) const;
         BigInteger  operator += (const BigInteger &T) { return *this = *this + T; }
         BigInteger  operator -  (const BigInteger &) const;
@@ -34,13 +37,19 @@ namespace high_precision
         BigInteger  operator ^  (const long long &) const;
         BigInteger  operator ^= (const long long &b) { return *this = *this ^ b; }
         bool        operator >  (const BigInteger &T) const;
-        bool        operator >  (const long long &b) const { return *this > BigInteger(b); };
-        bool        operator <  (const BigInteger &T) const;
-        bool        operator <  (const long long &b) const { return *this < BigInteger(b); };
+        bool        operator <  (const BigInteger &T) const { return T > *this; };
+        bool        operator >= (const BigInteger &T) const { return !(T > *this); }
+        bool        operator <= (const BigInteger &T) const { return !(*this > T); }
         bool        operator == (const BigInteger &T) const;
-        bool        operator == (const long long &b) const { return *this == BigInteger(b); };
+
+        void to_inner_char() const;
+        char * get_char() const { return buffer + (buffer[0] == '0'); }
+        char * to_char() const { to_inner_char(); return get_char(); }
+        void get_raw_sqrt() const;
+        BigInteger self_sqrt() const { get_raw_sqrt(); return BigInteger(raw_sqrt); }
 
         void print(bool stdio=true);
+        void println(bool stdio=true) { print(stdio); if (stdio) puts(""); else cout << endl; }
     };
 
     BigInteger::BigInteger(const long long b) : a()
@@ -74,20 +83,22 @@ namespace high_precision
     BigInteger::BigInteger(const char *s) : a()
     {
         int t, k, index, l, i;
+        bool flag = false;
         memset(a, 0, sizeof a);
+        if (s[0] == '-') flag = true, ++s;
         l = (int) strlen(s);
         length = l / UNIT_LENGTH;
-        if (l % UNIT_LENGTH) length++;
+        if (l % UNIT_LENGTH) ++length;
         index = 0;
         for (i = l - 1; i >= 0; i -= UNIT_LENGTH)
         {
             t = 0;
             k = i - UNIT_LENGTH + 1;
             if (k < 0) k = 0;
-            for (int j = k; j <= i; ++j)
-                t = t * 10 + s[j] - '0';
+            for (int j = k; j <= i; ++j) t = t * 10 + s[j] - '0';
             a[index++] = t;
         }
+        if (flag) a[index - 1] = -a[index - 1];
     }
 
     BigInteger BigInteger::operator+(const BigInteger &T) const
@@ -121,34 +132,34 @@ namespace high_precision
                 while (t1.a[j] == 0) ++j;
                 --t1.a[j--];
                 while (j > i) t1.a[j--] += UNIT_MAX;
-                t1.a[i] += UNIT_MAX + 1 - t2.a[i];
+                t1.a[i] += UNIT_SIZE - t2.a[i];
             }
             else t1.a[i] -= t2.a[i];
         }
         t1.length = big;
-        while (t1.a[t1.length - 1] == 0 && t1.length > 1) t1.length--, big--;
-        if (flag) t1.a[big - 1] = 0 - t1.a[big - 1];
+        while (t1.a[t1.length - 1] == 0 && t1.length > 1) --t1.length, --big;
+        if (flag) t1.a[big - 1] = -t1.a[big - 1];
         return t1;
     }
 
     BigInteger BigInteger::operator*(const BigInteger &T) const
     {
         BigInteger ret;
-        int i = 0, j = 0, up;
-        int temp, temp1;
+        int i = 0, j = 0, up, temp1;
+        long long temp;
         for (i = 0; i < length; ++i)
         {
             up = 0;
             for (j = 0; j < T.length; ++j)
             {
-                temp = a[i] * T.a[j] + ret.a[i + j] + up;
+                temp = (long long) a[i] * T.a[j] + ret.a[i + j] + up;
                 if (temp > UNIT_MAX)
                 {
-                    temp1 = temp - temp / (UNIT_SIZE) * (UNIT_SIZE);
-                    up = temp / (UNIT_SIZE);
+                    temp1 = (int) (temp - temp / UNIT_SIZE * UNIT_SIZE);
+                    up = (int) (temp / UNIT_SIZE);
                     ret.a[i + j] = temp1;
                 }
-                else up = 0, ret.a[i + j] = temp;
+                else up = 0, ret.a[i + j] = (int) temp;
             }
             if (up != 0) ret.a[i + j] = up;
         }
@@ -159,15 +170,16 @@ namespace high_precision
 
     BigInteger BigInteger::operator/(const long long &b) const
     {
+        assert(b != 0);
         BigInteger ret;
         long long i, down = 0;
         for (i = length - 1; i >= 0; i--)
         {
-            ret.a[i] = (int) ((down * (UNIT_SIZE) + a[i]) / b);
-            down = a[i] + down * (UNIT_SIZE) - b * ret.a[i] ;
+            ret.a[i] = (int) ((down * UNIT_SIZE + a[i]) / b);
+            down = a[i] + down * UNIT_SIZE - b * ret.a[i] ;
         }
         ret.length = length;
-        while (ret.a[ret.length - 1] == 0 && ret.length > 1) ret.length--;
+        while (ret.a[ret.length - 1] == 0 && ret.length > 1) --ret.length;
         return ret;
     }
 
@@ -191,24 +203,29 @@ namespace high_precision
 
     bool BigInteger::operator>(const BigInteger &T) const
     {
-        if (length > T.length) return true;
+        if (a[length - 1] < 0)
+        {
+            if (T.a[T.length - 1] < 0)
+            {
+                if (length < T.length) return true;
+                if (length == T.length)
+                {
+                    int ln = length - 1;
+                    if (a[ln] > T.a[ln]) return true;
+                    if (a[ln] < T.a[ln]) return false;
+                    --ln;
+                    while (a[ln] == T.a[ln] && ln >= 0) --ln;
+                    return ln >= 0 && a[ln] < T.a[ln];
+                }
+            }
+            return false;
+        }
+        if (T.a[length - 1] < 0 || length > T.length) return true;
         if (length == T.length)
         {
             int ln = length - 1;
             while (a[ln] == T.a[ln] && ln >= 0) --ln;
             return ln >= 0 && a[ln] > T.a[ln];
-        }
-        return false;
-    }
-
-    bool BigInteger::operator<(const BigInteger &T) const
-    {
-        if (length < T.length) return true;
-        if (length == T.length)
-        {
-            int ln = length - 1;
-            while (a[ln] == T.a[ln] && ln >= 0) --ln;
-            return ln >= 0 && a[ln] < T.a[ln];
         }
         return false;
     }
@@ -222,6 +239,54 @@ namespace high_precision
             return ln == -1;
         }
         return false;
+    }
+
+    void BigInteger::to_inner_char() const
+    {
+        int ln = length - 1, cnt = 1, tmp, i;
+        if (a[ln] < 0) buffer[0] = '-', tmp = -a[ln];
+        else buffer[0] = '0', tmp = a[ln];
+        for (i = UNIT_SIZE / 10; a[ln] / i == 0; i /= 10);
+        for (; i; i /= 10)
+            buffer[cnt++] = (char) ('0' + tmp / i), tmp %= i;
+        for (--ln; ln >= 0; --ln)
+        {
+            tmp = a[ln];
+            for (i = UNIT_SIZE / 10; i; i /= 10)
+                buffer[cnt++] = (char) ('0' + tmp / i), tmp %= i;
+        }
+        buffer[cnt] = '\0';
+    }
+
+    int sqrt_dfs(int o, char *O, int I)
+    {
+        char c, *D = O;
+        if(o > 0)
+        {
+            for(SQRT_L = 0; D[SQRT_L]; D[SQRT_L++] -= 10)
+            {
+                D[SQRT_L++] -= 120;
+                D[SQRT_L] -= 110;
+                while(!sqrt_dfs(0, O, SQRT_L)) D[SQRT_L] += 20;
+                raw_sqrt[SQRT_CNT++] = (char) ((D[SQRT_L] + 1032) / 20);
+            }
+            raw_sqrt[SQRT_CNT] = '\0';
+        }
+        else
+        {
+            c = (char) (o + (D[I] + 82) % 10 - (I > SQRT_L >> 1) * (D[I - SQRT_L + I] + 72) / 10 - 9);
+            D[I] += I < 0 ? 0 : !(o = sqrt_dfs(c / 10, O, I - 1)) * ((c + 999) % 10 - (D[I] + 92) % 10);
+        }
+        return o;
+    }
+
+    void BigInteger::get_raw_sqrt() const
+    {
+        assert(a[length - 1] >= 0);
+        to_inner_char();
+        _debug(buffer)
+        SQRT_CNT = 0;
+        sqrt_dfs(2, buffer + (strlen(buffer) & 1), 0);
     }
 
     void BigInteger::print(bool stdio)
@@ -245,7 +310,6 @@ namespace high_precision
                 }
                 printf("%s", ch);
             }
-            puts("");
         }
         else
         {
@@ -256,7 +320,6 @@ namespace high_precision
                 cout.fill('0');
                 cout << a[i];
             }
-            cout << endl;
         }
     }
 }
